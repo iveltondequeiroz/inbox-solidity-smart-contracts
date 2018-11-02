075d4a209e8e8ce36ec1cf1a8f447addcfa0d879
@ivelton2018 <ivelton2018@gmail.com>
2018-11-02 15:25:20 -0400

    test fetching ganache accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,24 @@
 const assert = require('assert')
-const ganache = require('ganahce-cli')
+const ganache = require('ganache-cli')
 const Web3 = require('web3')
+
+// web instance
+const web3 = new Web3(ganache.provider())
+
+// testing
+beforeEach(async ()=> {
+	// get accounts
+	accounts = await web3.eth.getAccounts()
+	console.log(accounts)
+	// deploy contract to account
+})
+
+describe('Inbox', ()=> {
+	it('deploys a contract', ()=> {
+
+	})	
+})
+
+
+
 
