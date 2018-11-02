0f61024a66389900bfb240aada4ed9834d69da10
@ivelton2018 <ivelton2018@gmail.com>
2018-11-02 14:30:24 -0400

    add web3 ganache mocha

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## INBOX Solidity Smart Contracts
+
+- node scripts
+- solc
+- ganache-cli
+- web3.js
+- mocha
---
+++ test/Inbox.test.js
@@ -0,0 +1,4 @@
+const assert = require('assert')
+const ganache = require('ganahce-cli')
+const Web3 = require('web3')
+
